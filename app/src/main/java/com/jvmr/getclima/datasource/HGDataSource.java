b726a25af01891b7d6df57053f365fcefb2ad10d
Demetrius <demetrius.mp789@gmail.com>
2021-06-19 15:36:55 -0400

    Finalizando modelo JSON parse da cidade, e das previsões

--- app/src/main/java/com/jvmr/getclima/datasource/HGDataSource.java
+++ app/src/main/java/com/jvmr/getclima/datasource/HGDataSource.java
@@ -1,6 +1,5 @@
 package com.jvmr.getclima.datasource;
 
-import com.google.gson.Gson;
 import com.jvmr.getclima.model.CidadeModel;
 import com.jvmr.getclima.service.HttpService;
 
@@ -12,7 +11,6 @@ import java.util.concurrent.ExecutionException;
 public class HGDataSource {
     static final private String baseURL = "https://api.hgbrasil.com/weather/";
     static final private String key = "3a0b1b47";
-    final private Gson gson = new Gson();
 
     private static HGDataSource instance;
 
@@ -32,7 +30,6 @@ public class HGDataSource {
             JSONObject reader = new JSONObject(retorno.toString());
             JSONObject results = reader.getJSONObject("results");
             cidadeModel = CidadeModel.readJSON(results);
-            // cidadeModel = gson.fromJson(retorno.toString(), CidadeModel.class);
         } catch (InterruptedException | ExecutionException | JSONException e) {
             e.printStackTrace();
         }
