873c4c78c1467e782a87dcbcdcc0a43bc7c9979e
unknown <maria.lbcs@icloud.com>
2021-06-28 13:33:22 -0400

    Tratando possíveis campos vazios ao logar

--- app/src/main/java/com/jvmr/getclima/view/LoginActivity.java
+++ app/src/main/java/com/jvmr/getclima/view/LoginActivity.java
@@ -57,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         email = edtEmail.getEditText().getText().toString();
         senha = edtSenha.getEditText().getText().toString();
 
+        //Tratamento caso o usuário tente logar e os campos estiverem vazios
         if(email.equals("") && senha.equals("")){
             Toast.makeText(LoginActivity.this, "É necessário preencher todos os campos",
                     Toast.LENGTH_LONG).show();
