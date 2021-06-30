2d8064112a3e6cb93f7a8d9daeee6e5394274f8c
unknown <maria.lbcs@icloud.com>
2021-06-29 21:41:57 -0400

    Implementando CRUD das cidades

--- app/src/main/java/com/jvmr/getclima/view/CidadesFragment.java
+++ app/src/main/java/com/jvmr/getclima/view/CidadesFragment.java
@@ -19,20 +19,31 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jvmr.getclima.R;
 import com.jvmr.getclima.model.CidadeModel;
+import com.jvmr.getclima.model.UsuarioModel;
+import com.jvmr.getclima.service.UsuarioService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class CidadesFragment extends Fragment {
+    private FirebaseFirestore db;
+    private FirebaseAuth fbAuth;
     private Spinner spnrCidades, spnrEstados;
     private ListView lvCidadesAdd;
     private TextView txtAddCidade;
     private ImageButton ibtnAddCidade;
     private String novaCidade;
+    private UsuarioService userInstance;
 
 
     @Nullable
@@ -51,6 +62,10 @@ public class CidadesFragment extends Fragment {
         lvCidadesAdd = (ListView) view.findViewById(R.id.lvCidades);
         txtAddCidade = view.findViewById(R.id.txtAddCidade);
         ibtnAddCidade = view.findViewById(R.id.ibtnAddCidade);
+        userInstance = UsuarioService.getInstance();
+
+        fbAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         ibtnAddCidade.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +79,7 @@ public class CidadesFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 String estadoSelecionado =  spnrEstados.getSelectedItem().toString();
                 List<String> cidades = CidadeModel.getCidadesPorEstado(estadoSelecionado);
-                //====Popula o spinner de estados===
+
                 ArrayAdapter<String> adpt_cidades = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, cidades);
                 spnrCidades.setAdapter(adpt_cidades);
                 novaCidade = spnrCidades.getSelectedItem().toString();
@@ -106,31 +121,76 @@ public class CidadesFragment extends Fragment {
 
 
     public void cidadesAdicionadas(){
-        //TODO: recupera as cidades adicionadas do usuário
-
-        //recupera do banco as cidades ja adicionadas pelo usuário
-
-        //Tratamento caso o usu[ario ainda náo tenha cidades adicionadas
-        /**if(cidades.isNull()){
+        List <String> cidadesAdd = new ArrayList<>();
+        cidadesAdd = userInstance.getUsuarioModel().getCidadesIds();
+        if(cidadesAdd.isEmpty()){
             Toast.makeText(getActivity(), "Você ainda não tem cidades adicionadas!!",
                     Toast.LENGTH_LONG).show();
             return;
-        }**/
-
-        //====Popula o listView====
-        //CostumeArrayAdapter adpt = new CostumeArrayAdapter(requireActivity(), cidadesAdd);
-        //lvCidadesAdd.setAdapter(adpt);
+        }
+        CostumeArrayAdapter adpt = new CostumeArrayAdapter(requireActivity(), cidadesAdd);
+        lvCidadesAdd.setAdapter(adpt);
     }
 
     public void addCidade(){
-        //TODO: adiciona ao banco a cidade adicionada
+        UsuarioModel usuarioModel = userInstance.getUsuarioModel();
+        List <String> cidadesAdd =usuarioModel.getCidadesIds();
 
-        //verifica se a cidade a ser adicionada já esta na lista de cidades do usuário
+        cidadesAdd.add(novaCidade);
+        usuarioModel.setCidadesIds(cidadesAdd);
 
-        //adicona cidades no banco do usário
-        Toast.makeText(getActivity(), "Cidade adicionada com sucesso!!",
-                Toast.LENGTH_LONG).show();
-        txtAddCidade.setText("Escolha uma cidade");
+        FirebaseUser user = fbAuth.getCurrentUser();
+        assert user != null;
+        db.collection("users")
+                .document(user.getUid())
+                .set(usuarioModel.toMap())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        cidadesAdicionadas();
+                        Toast.makeText(getActivity(), "Cidade adicionada com sucesso :)",
+                                Toast.LENGTH_LONG).show();
+                        txtAddCidade.setText("Escolha uma cidade");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(),
+                                "Não foi possível adiconar a cidade :(",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    public void selecionaCidade(){
+        lvCidadesAdd.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO: Consulta api e passa infos para InicioFragment
+            }
+        });
+    }
+
+    public void deletaCidade(){
+        CostumeArrayAdapter.ViewHolder vh = new CostumeArrayAdapter.ViewHolder();
+        CostumeArrayAdapter.getIcon(vh, getView()).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),
+                        "TESTANDOOOO",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        UsuarioService userInstance = UsuarioService.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth fbAuth = FirebaseAuth.getInstance();
+
+        List<String> cidades = userInstance.getUsuarioModel().getCidadesIds();
+
     }
 
 
