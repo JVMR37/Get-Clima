031e24be67cc38cfcf12ed2dad9c1d42c998e5f0
unknown <maria.lbcs@icloud.com>
2021-06-30 02:24:05 -0400

    Definindo ação do botão de exclusão

--- app/src/main/java/com/jvmr/getclima/view/CostumeArrayAdapter.java
+++ app/src/main/java/com/jvmr/getclima/view/CostumeArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,10 +13,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.jvmr.getclima.R;
+import com.jvmr.getclima.model.UsuarioModel;
+import com.jvmr.getclima.service.UsuarioService;
 
 import java.util.List;
 
 public class CostumeArrayAdapter  extends ArrayAdapter {
+    private CostumeArrayAdapter adpt;
     private List<String> cidades;
     private Context context;
 
@@ -39,11 +43,13 @@ public class CostumeArrayAdapter  extends ArrayAdapter {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         ViewHolder vh = new ViewHolder();
+
         if(convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listview_item, parent, false);
 
             vh.cidade = (TextView) convertView.findViewById(R.id.txtCidade);
+            vh.btnRemove = (ImageButton) convertView.findViewById(R.id.imgRemove);
 
             convertView.setTag(vh);
         }
@@ -53,17 +59,22 @@ public class CostumeArrayAdapter  extends ArrayAdapter {
         }
 
         vh.cidade.setText(cidades.get(position));
-
+        vh.btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CidadesFragment.removeCidade(adpt, cidades.get(position), cidades);
+            }
+        });
         return convertView;
     }
 
     static class ViewHolder{
         TextView cidade;
-        ImageView img_trash;
+        ImageButton btnRemove;
     }
 
-    public static ImageView getIcon(ViewHolder vh, View view){
-        return vh.img_trash = (ImageView) view.findViewById(R.id.imgRemove);
+    public void setAdpt(CostumeArrayAdapter apdt){
+        this.adpt = apdt;
     }
 
 }
