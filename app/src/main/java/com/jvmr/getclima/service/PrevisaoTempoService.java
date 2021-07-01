74b2fda01ff83d275f59abee4e3e49cb630a44a4
unknown <maria.lbcs@icloud.com>
2021-07-01 03:17:29 -0400

    Singleton para previsões

--- app/src/main/java/com/jvmr/getclima/service/PrevisaoTempoService.java
+++ app/src/main/java/com/jvmr/getclima/service/PrevisaoTempoService.java
@@ -1,2 +1,25 @@
-package com.jvmr.getclima.service;public class PrevisaoTempoService {
+package com.jvmr.getclima.service;
+
+import com.jvmr.getclima.model.CidadeModel;
+
+public class PrevisaoTempoService {
+
+    static private PrevisaoTempoService instance;
+
+    private CidadeModel cidadeModel;
+
+    public static PrevisaoTempoService getInstance() {
+        if (instance == null) {
+            instance = new PrevisaoTempoService();
+        }
+        return instance;
+    }
+
+    public CidadeModel getCidadeModel() {
+        return cidadeModel;
+    }
+
+    public void setCidadeModel(CidadeModel cidadeModel) {
+        this.cidadeModel = cidadeModel;
+    }
 }
